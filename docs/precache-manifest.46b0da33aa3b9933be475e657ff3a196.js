df10ab9ce0042ddec4e3e8340a219b9a8ef9343f
mylostandromeda <49440391+mylostandromeda@users.noreply.github.com>
2020-07-27 20:06:28 +0300

    add build

---
+++ docs/precache-manifest.46b0da33aa3b9933be475e657ff3a196.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289db9ed9842e4662a48a91b695e724c",
+    "url": "/react-school/index.html"
+  },
+  {
+    "revision": "9c8be07edec6be823760",
+    "url": "/react-school/static/css/2.fddaa3a5.chunk.css"
+  },
+  {
+    "revision": "27e7b3b791309a9eec28",
+    "url": "/react-school/static/css/main.fa3dd513.chunk.css"
+  },
+  {
+    "revision": "9c8be07edec6be823760",
+    "url": "/react-school/static/js/2.9ff2a7d8.chunk.js"
+  },
+  {
+    "revision": "40006afefec11bed70158a039388e3df",
+    "url": "/react-school/static/js/2.9ff2a7d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e7b3b791309a9eec28",
+    "url": "/react-school/static/js/main.4705666d.chunk.js"
+  },
+  {
+    "revision": "b5729f031508e5911832",
+    "url": "/react-school/static/js/runtime-main.8c687318.js"
+  }
+]);
